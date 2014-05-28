b582e3c0506949fc64f74889839e70fed72b263f
michaesh <michaesh@usc.edu>
2014-05-27 23:01:21 -0700

    Added declarator and movement method to gui

--- src/gui/AgentGui.java
+++ src/gui/AgentGui.java
@@ -1,5 +1,26 @@
 package gui;
 
-public class AgentGui {
+import agent.*;
 
+public class AgentGui {
+	Agent myAgent;
+	int xloc, yloc;
+	
+	/* Declarator, input: corresponding agent 
+	 * */
+	public AgentGui (Agent theAgent, int x, int y){
+		myAgent = theAgent;
+		xloc = x;
+		yloc = y;
+	}
+	
+	/* Moving function for Gui image, input: movement in 2 axis
+	 * */
+	public void move(int x, int y){
+		xloc += x;
+		yloc += y;
+	}
+
+
+	
 }
